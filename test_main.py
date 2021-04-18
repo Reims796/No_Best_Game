7c7769b10d55fe72e0f71c322c55b92db1de725e
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-18 18:14:36 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,10 +7,11 @@ from PIL import Image
 
 
 class Window:
-    def __init__(self, size, name):
+    def __init__(self, size, name, pos_b):
         self.size = size
         self.name = name
         self.window = sdl2.ext.Window(self.name, size=self.size)
+        self.pos_b = pos_b
 
     def fill_Window(self, color):
         r, g, b = color
@@ -50,67 +51,67 @@ class Window:
         Window.rectangle(self, x + 25, y + 15, 5, 5, color2)
         Window.rectangle(self, x + 30, y + 15, 5, 5, color2)
         Window.rectangle(self, x + 30, y + 20, 5, 5, color2)
+ # sp = [(0, 0, 0), (255, 255, 255), (245, 3, 18), (150, 0, 18), (99, 2, 17), (8, 198, 245), (245, 129, 5)]
+    def boat_contour(self, x, y, sp):
+        Window.rectangle(self, x, y, 8, 8, sp[0])
+        Window.rectangle(self, x - 8, y + 8, 8, 16, sp[0])
+        Window.rectangle(self, x + 8, y + 8, 8, 16, sp[0])
+        Window.rectangle(self, x - 16, y + 24, 8, 8, sp[0])
+        Window.rectangle(self, x + 16, y + 24, 8, 8, sp[0])
+        Window.rectangle(self, x - 24, y + 32, 8, 8, sp[0])
+        Window.rectangle(self, x + 24, y + 32, 8, 8, sp[0])
+        Window.rectangle(self, x - 32, y + 40, 8, 16, sp[0])
+        Window.rectangle(self, x + 32, y + 40, 8, 16, sp[0])
+        Window.rectangle(self, x - 40, y + 56, 8, 104, sp[0])
+        Window.rectangle(self, x + 40, y + 56, 8, 104, sp[0])
+        Window.rectangle(self, x - 40, y + 160, 88, 8, sp[0])
+        Window.rectangle(self, x - 8, y + 32, 24, 24, sp[0])
+        Window.rectangle(self, x - 8, y + 64, 24, 8, sp[0])
+        Window.rectangle(self, x - 24, y + 72, 56, 80, sp[0])
 
-    def boat_contour(self, x, y):
-        Window.rectangle(self, x, y, 8, 8, (0, 0, 0))
-        Window.rectangle(self, x - 8, y + 8, 8, 16, (0, 0, 0))
-        Window.rectangle(self, x + 8, y + 8, 8, 16, (0, 0, 0))
-        Window.rectangle(self, x - 16, y + 24, 8, 8, (0, 0, 0))
-        Window.rectangle(self, x + 16, y + 24, 8, 8, (0, 0, 0))
-        Window.rectangle(self, x - 24, y + 32, 8, 8, (0, 0, 0))
-        Window.rectangle(self, x + 24, y + 32, 8, 8, (0, 0, 0))
-        Window.rectangle(self, x - 32, y + 40, 8, 16, (0, 0, 0))
-        Window.rectangle(self, x + 32, y + 40, 8, 16, (0, 0, 0))
-        Window.rectangle(self, x - 40, y + 56, 8, 104, (0, 0, 0))
-        Window.rectangle(self, x + 40, y + 56, 8, 104, (0, 0, 0))
-        Window.rectangle(self, x - 40, y + 160, 88, 8, (0, 0, 0))
-        Window.rectangle(self, x - 8, y + 32, 24, 24, (0, 0, 0))
-        Window.rectangle(self, x - 8, y + 64, 24, 8, (0, 0, 0))
-        Window.rectangle(self, x - 24, y + 72, 56, 80, (0, 0, 0))
+        Window.rectangle(self, x, y + 40, 8, 8, sp[1])
+        Window.rectangle(self, x, y + 72, 8, 8, sp[1])
+        Window.rectangle(self, x - 8, y + 80, 8, 8, sp[1])
 
-        Window.rectangle(self, x, y + 40, 8, 8, (255, 255, 255))
-        Window.rectangle(self, x, y + 72, 8, 8, (255, 255, 255))
-        Window.rectangle(self, x - 8, y + 80, 8, 8, (255, 255, 255))
+    def boat_red(self, x, y, sp):
+        Window.rectangle(self, x - 8, y + 24, 8, 8, sp[2])
+        Window.rectangle(self, x - 16, y + 32, 8, 40, sp[2])
+        Window.rectangle(self, x - 24, y + 40, 8, 32, sp[2])
+        Window.rectangle(self, x - 32, y + 56, 8, 8 * 13, sp[2])
+        Window.rectangle(self, x - 32, y + 152, 24, 8, sp[2])
 
-    def boat_red(self, x, y):
-        Window.rectangle(self, x - 8, y + 24, 8, 8, (245, 3, 18))
-        Window.rectangle(self, x - 16, y + 32, 8, 40, (245, 3, 18))
-        Window.rectangle(self, x - 24, y + 40, 8, 32, (245, 3, 18))
-        Window.rectangle(self, x - 32, y + 56, 8, 8 * 13, (245, 3, 18))
-        Window.rectangle(self, x - 32, y + 152, 24, 8, (245, 3, 18))
+        Window.rectangle(self, x, y + 8, 8, 24, sp[3])
+        Window.rectangle(self, x + 8, y + 24, 8, 8, sp[3])
+        Window.rectangle(self, x - 8, y + 56, 24, 8,sp[3])
+        Window.rectangle(self, x + 16, y + 32, 8, 40, sp[3])
+        Window.rectangle(self, x + 24, y + 40, 8, 32, sp[3])
+        Window.rectangle(self, x + 32, y + 56, 8, 16, sp[3])
+        Window.rectangle(self, x - 8, y + 152, 40, 8, sp[3])
 
-        Window.rectangle(self, x, y + 8, 8, 24, (150, 0, 18))
-        Window.rectangle(self, x + 8, y + 24, 8, 8, (150, 0, 18))
-        Window.rectangle(self, x - 8, y + 56, 24, 8, (150, 3, 18))
-        Window.rectangle(self, x + 16, y + 32, 8, 40, (150, 3, 18))
-        Window.rectangle(self, x + 24, y + 40, 8, 32, (150, 3, 18))
-        Window.rectangle(self, x + 32, y + 56, 8, 16, (150, 3, 18))
-        Window.rectangle(self, x - 8, y + 152, 40, 8, (150, 3, 18))
+        Window.rectangle(self, x + 32, y + 72, 8, 88, sp[4])
 
-        Window.rectangle(self, x + 32, y + 72, 8, 88, (99, 2, 17))
+    def boat_color(self, x, y, sp):
+        Window.rectangle(self, x - 8, y + 72, 8, 8, sp[5])
+        Window.rectangle(self, x + 8, y + 72, 8, 24, sp[5])
+        Window.rectangle(self, x - 16, y + 80, 8, 24, sp[5])
+        Window.rectangle(self, x, y + 80, 8, 16, sp[5])
+        Window.rectangle(self, x + 16, y + 80, 8, 24, sp[5])
+        Window.rectangle(self, x - 8, y + 88, 8, 8, sp[5])
 
-    def boat_color(self, x, y):
-        Window.rectangle(self, x - 8, y + 72, 8, 8, (8, 198, 245))
-        Window.rectangle(self, x + 8, y + 72, 8, 24, (8, 198, 245))
-        Window.rectangle(self, x - 16, y + 80, 8, 24, (8, 198, 245))
-        Window.rectangle(self, x, y + 80, 8, 16, (8, 198, 245))
-        Window.rectangle(self, x + 16, y + 80, 8, 24, (8, 198, 245))
-        Window.rectangle(self, x - 8, y + 88, 8, 8, (8, 198, 245))
+        Window.rectangle(self, x - 8, y + 96, 24, 8, sp[-1])
+        Window.rectangle(self, x, y + 104, 8, 16, sp[-1])
+        Window.rectangle(self, x - 16, y + 104, 8, 16, sp[-1])
+        Window.rectangle(self, x + 16, y + 104, 8, 16, sp[-1])
 
-        Window.rectangle(self, x - 8, y + 96, 24, 8, (245, 129, 5))
-        Window.rectangle(self, x, y + 104, 8, 16, (245, 129, 5))
-        Window.rectangle(self, x - 16, y + 104, 8, 16, (245, 129, 5))
-        Window.rectangle(self, x + 16, y + 104, 8, 16, (245, 129, 5))
-
-        Window.rectangle(self, x, y + 128, 8, 16, (245, 129, 5))
-        Window.rectangle(self, x - 16, y + 128, 8, 16, (245, 129, 5))
-        Window.rectangle(self, x + 16, y + 128, 8, 16, (245, 129, 5))
+        Window.rectangle(self, x, y + 128, 8, 16, sp[-1])
+        Window.rectangle(self, x - 16, y + 128, 8, 16, sp[-1])
+        Window.rectangle(self, x + 16, y + 128, 8, 16, sp[-1])
 
 
-    def draw_boat(self, x, y):
-        Window.boat_contour(self, x, y)
-        Window.boat_red(self, x, y)
-        Window.boat_color(self, x, y)
+    def draw_boat(self, x, y, sp):
+        Window.boat_contour(self, x, y, sp)
+        Window.boat_red(self, x, y, sp)
+        Window.boat_color(self, x, y, sp)
 
 
 
@@ -130,13 +131,17 @@ class Window:
                 elif event.type == sdl2.SDL_KEYDOWN:
                     if event.key.keysym.sym == sdl2.SDLK_SPACE:
                         Window.draw_bomb(self, 100, 100, (166, 166, 166), (255, 255, 255))
-                        Window.draw_boat(self, 540, 500)
+                        Window.draw_boat(self, self.pos_b[0], self.pos_b[1], [(0, 0, 0), (255, 255, 255), (245, 3, 18), (150, 0, 18), (99, 2, 17), (8, 198, 245), (245, 129, 5)])
+                    elif event.key.keysym.sym == sdl2.SDLK_LEFT:
+                        Window.draw_boat(self, self.pos_b[0], self.pos_b[1], [(43, 122, 168) for _ in range(8)])
+                        self.pos_b = [self.pos_b[0] - 50, self.pos_b[1]]
+                        Window.draw_boat(self, self.pos_b[0], self.pos_b[1], [(0, 0, 0), (255, 255, 255), (245, 3, 18), (150, 0, 18), (99, 2, 17), (8, 198, 245), (245, 129, 5)])
             self.window.refresh()
         return 0
 
 
 def main():
-    window = Window((1081, 721), "No Best Game")
+    window = Window((1081, 721), "No Best Game", [540, 500])
 
     window.run()
     # window.fill_Window((240, 0, 0))
